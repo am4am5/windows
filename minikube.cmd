01a1e30efdce4ca681afaf7c7d518c7c1274af0c
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-01-20 22:53:20 +0300

    Create minikube.cmd

---
+++ minikube.cmd
@@ -0,0 +1,6 @@
+m config set vm-driver hyperv 
+m config set container-runtime containerd
+m config set memory 3072
+m config set disk-size 10GB
+m config set kubernetes-version 1.17.1
+m config set insecure-registry "192.168.0.0/24"
