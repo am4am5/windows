a8fc44e6e8330c754ea5055b2cf44f4a41c0f82b
am4am5 <36809996+am4am5@users.noreply.github.com>
2020-02-25 19:46:25 +0300

    Update minikube.cmd

--- minikube.cmd
+++ minikube.cmd
@@ -1,6 +1,6 @@
 m config set vm-driver hyperv 
 m config set memory 3072
 m config set disk-size 10GB
-m config set kubernetes-version 1.17.1
+m config set kubernetes-version 1.17.3
 m config set insecure-registry "192.168.0.0/24"
 # m config set container-runtime containerd (no sc) / cri-o (no sc) / docker
